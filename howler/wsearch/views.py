6e0eba6e5115d2eacf18e8ae79cb7e88cf29597e
lukas <lukas.m.schmid@gmail.com>
2016-11-24 18:05:18 +0900

    Run python wally-search from django

--- howler/wsearch/views.py
+++ howler/wsearch/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from wally.elastic.search import Search
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the search index.")
+    res_strings = ["Hello, world. You're at the search index.<br><br>"]
+
+    res_strings.append("Search for Japanese text: 何か調整が必要でしょうか?<br><br>")
+    response = Search().search('何か調整が必要でしょうか?')
+
+    for hit in response:
+        if hasattr(hit, 'buildNum'):
+            continue
+        res_strings.append('<br><br><hr>{0}, {1}, {2} <br> {3}<hr>'.format(hit.meta.score, hit.langCode, hit.langPercent, hit.subject))
+        if hasattr(hit.meta.highlight, 'body'):
+            for fragment in hit.meta.highlight.body:
+                res_strings.append('{0}<br>'.format(fragment))
+        if hasattr(hit.meta.highlight, 'subject'):
+            for fragment in hit.meta.highlight.subject:
+                res_strings.append('{0}<br>'.format(fragment))
+
+    return HttpResponse(''.join(res_strings))
