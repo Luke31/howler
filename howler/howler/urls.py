134faf0a862dd8a1b7e70519e5bcdcdb69907423
lukas <lukas.m.schmid@gmail.com>
2016-12-01 16:53:24 +0900

    Removed mostly wally from howler (single naming on webpage)

--- howler/howler/urls.py
+++ howler/howler/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^wally/', include('wally_search.urls')),
+    url(r'^', include('wally_search.urls')),
     url(r'^admin/', admin.site.urls),
 ]
