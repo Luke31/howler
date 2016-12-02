96d5fbc8e4a9671f226014449e7864064a4401a2
lukas <lukas.m.schmid@gmail.com>
2016-12-02 11:53:37 +0900

    django different pw

--- howler/howler/settings.py
+++ howler/howler/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'hogehoge',
+        'PASSWORD': 'django',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
