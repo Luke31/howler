707c3e8f9920cce098b16dffd6ce1abea192cbd1
lukas <lukas.m.schmid@gmail.com>
2016-12-02 11:43:57 +0900

    fabfile: updated execution dir

--- howler/fabfile.py
+++ howler/fabfile.py
@@ -24,7 +24,7 @@ def deploy():
 
 
 def pip_install():
-    with cd(code_dir):
+    with cd(env.project_root):
         run('pip3 install -r requirements.txt')
         run(os.path.join(python3_dir, 'pip3') + ' install -r requirements.txt')
 
