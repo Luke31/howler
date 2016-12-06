1ad5d5bbb5ee4413d5c7b2167dfe662264175069
lukas <lukas.m.schmid@gmail.com>
2016-12-06 16:06:16 +0900

    don't delete old index

--- wally/main-index.py
+++ wally/main-index.py
@@ -2,7 +2,7 @@ import os
 from wally.elastic.index import Index
 
 index = Index()
-index.add_mapping_to_index_multi(delete_old_indices=True)
+index.add_mapping_to_index_multi(delete_old_indices=False)
 
 summary = index.index_bulk_from_dir(os.getcwd())
 print("Successfully indexed {0}/{1} emails. Errors on json-convert: {2}, Errors in indexing: {3}".format(
