2a30bcb1696db8c9876ba5a08797f08356ecb267
lukas <lukas.m.schmid@gmail.com>
2016-12-08 17:04:00 +0900

    move

--- wally/move.py
+++ wally/move.py
@@ -19,6 +19,9 @@ def rename(file_name, dst, num=1):
 def copy_files(src, dst, file_list):
     for files in file_list:
         src_file_path = os.path.join(src, files)
+        if ~os.path.exists(src_file_path):
+            print("{0} doesn't exist".format(src_file_path))
+            continue
         dst_file_path = os.path.join(dst, files)
         if os.path.exists(dst_file_path):
             new_file_name = rename(files, dst)
