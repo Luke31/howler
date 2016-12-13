e770ef395331f35f357e1ea13fb212bf3403f7fe
lukas <lukas.m.schmid@gmail.com>
2016-12-13 11:50:01 +0900

    swap userdict adding before index update

--- howler/settings/views.py
+++ howler/settings/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.conf import settings as djsettings
 from elasticsearch import Elasticsearch
+import os
 
 
 class SynonymIndex(generic.ListView):
@@ -38,16 +39,19 @@ def synonym_delete(request, pk):
 
 def synonym_import(request):
     obj_list = Synonym.objects.order_by('synonym_term_a')
+
+    # Add terms to userdict
+    if os.path.exists(djsettings.ES_CONFIG_ROOT):
+        for x in obj_list:
+            ud = Userdict(es_conf_root=djsettings.ES_CONFIG_ROOT)
+            ud.update_term_to_userdict(x.synonym_term_b, x.synonym_term_b_katakana)
+
+    # Update index
     kuromoji_synonyms = [x.synonyms_combo() for x in obj_list]
     es = Elasticsearch(djsettings.ES_HOSTS, timeout=djsettings.ES_TIMEOUT, maxsize=djsettings.ES_MAXSIZE_CON)
     index = Index(es, es_index_prefix=djsettings.ES_INDEX_PREFIX, es_type_name=djsettings.ES_TYPE_NAME,
                   user_dictionary_file=djsettings.JA_USER_DICT)
     index.add_mapping_to_index_multi(delete_old_indices=False, kuromoji_synonyms=kuromoji_synonyms)  # Update index
-
-    # Add terms to userdict
-    for x in obj_list:
-        ud = Userdict(es_conf_root=djsettings.ES_CONFIG_ROOT)
-        ud.update_term_to_userdict(x.synonym_term_b, x.synonym_term_b_katakana)
 
     # return HttpResponse()
     return HttpResponseRedirect(reverse('settings:synonym'))  # Redirect to synonyms
