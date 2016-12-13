2e52774ce625119d92cd08015cd37093b101bd88
lukas <lukas.m.schmid@gmail.com>
2016-12-13 11:43:55 +0900

    helptext userdict

--- howler/settings/models.py
+++ howler/settings/models.py
@@ -7,7 +7,9 @@ from django.utils.translation import ugettext_lazy as _
 class Synonym(models.Model):
     synonym_term_a = models.CharField(max_length=200, verbose_name=_('Synonym Term A'))
     synonym_term_b = models.CharField(max_length=200, verbose_name=_('Synonym Term B (shortcut)'))
-    synonym_term_b_katakana = models.CharField(max_length=200, verbose_name=_('Synonym Term B Katakana'), default='')
+    synonym_term_b_katakana = models.CharField(max_length=200, verbose_name=_('Synonym Term B Katakana'), default='',
+                                               help_text=_(
+                                                   'If provided, term will be added to userdict (Required if shortcut)'))
 
     def synonyms_combo(self):
         return ', '.join((self.synonym_term_a, self.synonym_term_b))
