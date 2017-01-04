0e070612eb7e1a5ab6dfc5d5d7bd7ee565c0fd47
lukas <lukas.m.schmid@gmail.com>
2017-01-04 15:24:24 +0900

    Added kibana dashboard

--- howler/settings/context_processors.py
+++ howler/settings/context_processors.py
@@ -5,6 +5,7 @@ def global_settings(request):
     http_start = 'http://'
     es_host = ''.join((settings.ES_HOSTS[0], ':', str(settings.ES_DEFAULT_PORT)))
     kibana_host = ''.join((settings.ES_HOSTS[0], ':', '5601'))
+    dashboard_main = '/goto/bc0525132fd567e59cd41318099b547a'
     return {
         'http_es_settings': ''.join(
             (http_start, es_host, '/', settings.ES_INDEX_PREFIX.format('*'), '/_settings?pretty')),
@@ -12,6 +13,8 @@ def global_settings(request):
             (http_start, es_host, '/_cat/indices?v')),
         'http_kibana': ''.join(
             (http_start, kibana_host)),
+        'http_kibana_dashboard': ''.join(
+            (http_start, kibana_host, dashboard_main)),
         'http_kuromoji_analyze_test': ''.join(
             (http_start, es_host, '/', settings.ES_INDEX_PREFIX.format('ja'), '/_analyze?pretty=true&analyzer=kuromoji_custom')),
     }
